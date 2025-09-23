3be09b16947a4bef9628ed7e84cfc4774ea3917a
Noam Ram <noamramny@gmail.com>
2025-09-23 16:58:22 +0300

    Added port env

--- config/config.go
+++ config/config.go
@@ -11,15 +11,22 @@ import (
 type Config struct {
 	DatabaseURI string `validate:"required"`
 	LogPath     string `validate:"required"`
+	Port        int    `validate:"min=1,max=65535" env:"PORT" envDefault:"8080"`
 }
 
 func Load() (*Config, error) {
 	// Load .env into OS environment
 	_ = godotenv.Load()
 
+	port := 8080
+	if os.Getenv("PORT") != "" {
+		fmt.Sscanf(os.Getenv("PORT"), "%d", &port)
+	}
+
 	cfg := &Config{
 		DatabaseURI: os.Getenv("DATABASE_URI"),
 		LogPath:     os.Getenv("LOG_PATH"),
+		Port:        port,
 	}
 
 	validate := validator.New()
